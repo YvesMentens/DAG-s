aa8bb42e2e6765b35b321108eda23b58d6996fd1
YvesMentens <mentensyves@gmail.com>
2023-11-11 12:36:38 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ from airflow.operators.empty import EmptyOperator
 
 with DAG(
      dag_id="my_dag_name",
-     start_date=datetime.datetime(2021, 1, 1),
+     start_date=datetime.datetime(2023, 11, 11),
      schedule="@daily",
 ):
      EmptyOperator(task_id="task")
